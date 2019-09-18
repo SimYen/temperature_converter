7eec4b2227ff951027cda76c8d08747e75fa68ba
SYNG <simyenng@gmail.com>
2019-09-18 17:45:02 +0800

    complete part 1

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 console.log("hello script js");
 
+alert("Input temperature in fahrenheit");
+
 var inputHappened = function(currentInput){
-  console.log( currentInput );
-  var output = "WOW TEMPERATURE";
+  var temp = parseInt( currentInput );
+  var celsius = (temp - 32) * 5/9;
+  var kelvin = celsius + 273.15;
+  var output = Math.floor( celsius ) + " celsius " + Math.floor( kelvin ) + " kelvin" ;
   return output;
 };
