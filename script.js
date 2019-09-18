287be505e3490e57cd33f2c1c3403d44cf83f082
SYNG <simyenng@gmail.com>
2019-09-18 18:22:40 +0800

    check if input is NaN

--- script.js
+++ script.js
@@ -2,10 +2,25 @@ console.log("hello script js");
 
 alert("Input temperature in fahrenheit");
 
+//Part 1
 var inputHappened = function(currentInput){
-  var temp = parseInt( currentInput );
-  var celsius = (temp - 32) * 5/9;
-  var kelvin = celsius + 273.15;
-  var output = Math.floor( celsius ) + " celsius " + Math.floor( kelvin ) + " kelvin" ;
-  return output;
+  var temp = Number(currentInput);
+  console.log(temp);
+  if ( Number.isNaN(temp) ) {
+    var output = "Please enter a number";
+    return output;
+    } else {
+    var celsius = (temp - 32) * 5/9;
+    var kelvin = celsius + 273.15;
+    var output = celsius.toFixed(2) + " celsius " + kelvin.toFixed(2) + " kelvin" ;
+    return output;
+    }
 };
+
+/*
+var inputHappened = function(currentInput) {
+  var temp = parseInt( currentInput );
+  alert("Is temperature fahrenheit, celsius, or kelvin?");
+  var inputHappened = function(currentInput)
+}
+*/
